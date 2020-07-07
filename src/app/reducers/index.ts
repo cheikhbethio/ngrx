7c372b773f78285b117abfdf37460831ad0b883f
DDEAL\moussa <m.sow@deal.fr>
2020-07-07 16:13:45 +0200

    store dev tools

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,22 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { routerReducer } from '@ngrx/router-store';
 
 export interface AppState {
 
 }
 
-export const reducers: ActionReducerMap<AppState> = {
+const logger = function (reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('logger -> state, action', state, action);
+    return reducer(state, action);
+  };
+};
 
+export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer
 };
 
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
